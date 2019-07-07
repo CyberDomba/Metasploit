a761ca661cc08ac1d4b9e8fc183be8c41c0026c0
CyberDomba <50538340+CyberDomba@users.noreply.github.com>
2019-07-07 12:21:31 +0700

    Rename bash.sh.md to bash.sh

---
+++ bash.sh
@@ -0,0 +1,1 @@
+gem install bundle -y&&gem install bundler -y&&&pip2 install bundler&&bundle install -j5&&gem update --system&&bundle update nokogiri
--- bash.sh.md
+++
@@ -1,1 +0,0 @@
-gem install bundle -y&&gem install bundler -y&&&pip2 install bundler&&bundle install -j5&&gem update --system&&bundle update nokogiri
